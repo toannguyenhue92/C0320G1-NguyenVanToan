4369ac82437935d50fd7554184ac30c9eb10fea8
Toan Nguyen <toannguyen.hue92@gmail.com>
2020-05-07 14:04:20 +0700

    Datatypes in Java

---
+++ Module2/src/javalang/practice/JavaDataType.java
@@ -0,0 +1,18 @@
+package javalang.practice;
+
+public class JavaDataType {
+    public static void main(String[] args) {
+        int i = 10;
+        System.out.println("i = " + i);
+        float f = 20.5f;
+        System.out.println("f = " + f);
+        double d = 20.5;
+        System.out.println("d = " + d);
+        boolean b = true;
+        System.out.println("b = " + b);
+        char c = 'a';
+        System.out.println("c = " + c);
+        String s = "Huế";
+        System.out.println("s = " + s);
+    }
+}
