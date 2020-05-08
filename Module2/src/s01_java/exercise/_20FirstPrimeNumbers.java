29ecbc6dbc8a9e9a5abdf9e96b6331995e9d9b33
Toan Nguyen <toannguyen.hue92@gmail.com>
2020-05-08 21:57:43 +0700

    20 first prime numbers

---
+++ Module2/src/s01_java/exercise/_20FirstPrimeNumbers.java
@@ -0,0 +1,35 @@
+package s01_java.exercise;
+
+public class _20FirstPrimeNumbers {
+
+    public static void main(String[] args) {
+        int counter = 0;
+        int i = 0;
+        System.out.print("20 first prime numbers: ");
+        while (counter < 20) {
+            if (isPrimeNumber(i)) {
+                System.out.print(i);
+                counter++;
+                if (counter < 20) {
+                    System.out.print(", ");
+                }
+            }
+            i++;
+        }
+    }
+
+    static boolean isPrimeNumber(int number) {
+        if (number < 2) {
+            return false;
+        }
+        if (number == 2) {
+            return true;
+        }
+        for (int i = 2; i <= Math.sqrt(number); i++) {
+            if (number % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
