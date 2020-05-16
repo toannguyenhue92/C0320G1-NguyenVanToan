7f5d088a7dbb091a011aa5389bc435766ff0e215
Toan Nguyen <toannguyen.hue92@gmail.com>
2020-05-16 10:53:11 +0700

    fix

--- Module2/src/s05_abstract_and_interface/tutorial/circle_comparator/CircleComparator.java
+++ Module2/src/s05_abstract_and_interface/tutorial/circle_comparator/CircleComparator.java
@@ -8,12 +8,9 @@ public class CircleComparator implements Comparator<Circle> {
 
     @Override
     public int compare(Circle c1, Circle c2) {
-        if (c1.getRadius() > c2.getRadius())
-            return 1;
-        else if (c1.getRadius() < c2.getRadius())
-            return -1;
-        else
-            return 0;
+        if (c1.getRadius() > c2.getRadius()) return 2;
+        else if (c1.getRadius() < c2.getRadius()) return -2;
+        else return 0;
     }
 
 }
