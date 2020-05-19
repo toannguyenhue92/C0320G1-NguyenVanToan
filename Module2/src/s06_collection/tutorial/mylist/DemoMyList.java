ac5e0afe3395d8346f24f79aff90934db920c6b7
Toan Nguyen <toannguyen.hue92@gmail.com>
2020-05-19 14:35:38 +0700

    MyList

---
+++ Module2/src/s06_collection/tutorial/mylist/DemoMyList.java
@@ -0,0 +1,16 @@
+package s06_collection.tutorial.mylist;
+
+public class DemoMyList {
+    public static void main(String[] args) {
+        MyList<Integer> listInteger = new MyList<>();
+        listInteger.add(1);
+        listInteger.add(2);
+        listInteger.add(3);
+        listInteger.add(4);
+        listInteger.add(5);
+        System.out.println("element 4: " + listInteger.get(4));
+        System.out.println("element 1: " + listInteger.get(1));
+        System.out.println("element 2: " + listInteger.get(2));
+        System.out.println("element -1: " + listInteger.get(-1));
+    }
+}
