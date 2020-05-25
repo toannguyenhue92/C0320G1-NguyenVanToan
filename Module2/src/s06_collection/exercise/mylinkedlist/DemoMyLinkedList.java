c458ab1cbd8543e7cbd960519aa85b1a87976369
Toan Nguyen <toannguyen.hue92@gmail.com>
2020-05-25 11:07:51 +0700

    MyLinkedList

---
+++ Module2/src/s06_collection/exercise/mylinkedlist/DemoMyLinkedList.java
@@ -0,0 +1,17 @@
+package s06_collection.exercise.mylinkedlist;
+
+public class DemoMyLinkedList {
+    public static void main(String[] args) {
+        MyLinkedList myLinkedList = new MyLinkedList();
+        myLinkedList.add(0, 100);
+        myLinkedList.add(1, 5.6);
+        myLinkedList.add(2, true);
+        myLinkedList.add(3, "Zebra");
+        myLinkedList.addFirst("First");
+        myLinkedList.addFirst("First-1");
+        myLinkedList.addFirst("First-2");
+        myLinkedList.get(0).setData("Edited!");
+        myLinkedList.printList();
+        System.out.println("List size : " + myLinkedList.getSize());
+    }
+}
---
+++ Module2/src/s06_collection/exercise/mylinkedlist/Node.java
@@ -0,0 +1,26 @@
+package s06_collection.exercise.mylinkedlist;
+
+public class Node {
+    private Node next;
+    private Object data;
+
+    public Node(Object data) {
+        this.data = data;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
+
+    public Object getData() {
+        return data;
+    }
+
+    public void setData(Object data) {
+        this.data = data;
+    }
+}
