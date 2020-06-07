df9d1fd0a5f8668f90f57d73b76a653c611fb4ee
Toan Nguyen <toannguyen.hue92@gmail.com>
2020-06-07 21:24:57 +0700

    JSP and JSTL

---
+++ Module2_Web/02_JSP_JSTL/src/utilities/Calculator.java
@@ -0,0 +1,20 @@
+public class Calculator {
+    public double calculate(double firstOperand, double secondOperand, char operator)
+            throws RuntimeException {
+        switch (operator) {
+            case '+':
+                return firstOperand + secondOperand;
+            case '-':
+                return firstOperand - secondOperand;
+            case '*':
+                return firstOperand * secondOperand;
+            case '/':
+                if (secondOperand != 0)
+                    return firstOperand / secondOperand;
+                else
+                    throw new RuntimeException("Can't divide by zero");
+            default:
+                throw new RuntimeException("Invalid operation");
+        }
+    }
+}
